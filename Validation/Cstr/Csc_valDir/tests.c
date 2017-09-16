cabee08809958af1444983cb36fad9d70714f84b
Stephen Braithwaite <pleaseDontSpamMe@gmail.com>
2017-09-16 13:51:20 +1000

    Added some validation for cstr

---
+++ Validation/Cstr/Csc_valDir/tests.c
@@ -0,0 +1,76 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+#include <CscNetLib/std.h>
+#include <CscNetLib/alloc.h>
+#include <CscNetLib/cstr.h>
+
+FILE *fout;
+
+void printPassFail(const char *testName, csc_bool_t isPass)
+{	if (isPass)
+		fprintf(fout, "pass (%s)\n", testName);
+	else
+		fprintf(fout, "FAIL (%s)\n", testName);
+}
+
+#define longStr "aaabbbcccdddeeefffggghhhiiijjj"
+
+int main(int argc, char **argv)
+{
+	const char *testName;
+	fout = fopen("csc_testOut.txt", "a"); assert(fout);
+	csc_str_t *strA;
+
+	strA = csc_str_new(NULL);
+	printPassFail("NULLNew", csc_streq(csc_str_charr(strA),""));
+
+	csc_str_free(strA);
+	printPassFail("NULLFree", csc_mck_nchunks()==0);
+
+	strA = csc_str_new("");
+	printPassFail("EmptyNew", csc_streq(csc_str_charr(strA),""));
+
+	csc_str_free(strA);
+	printPassFail("EmptyFree", csc_mck_nchunks()==0);
+
+	strA = csc_str_new("a");
+	printPassFail("SingleNew", csc_streq(csc_str_charr(strA),"a"));
+
+	csc_str_free(strA);
+	printPassFail("SingleFree", csc_mck_nchunks()==0);
+
+	strA = csc_str_new(longStr);
+	printPassFail("longNew", csc_streq(csc_str_charr(strA),longStr));
+
+	csc_str_free(strA);
+	printPassFail("longFree", csc_mck_nchunks()==0);
+
+	strA = csc_str_new(NULL);
+	csc_str_append(strA, longStr);
+	csc_str_append_ch(strA, 'k');
+	csc_str_append(strA, longStr);
+	csc_str_append_ch(strA, 'k');
+	csc_str_append(strA, longStr);
+	csc_str_append_ch(strA, 'k');
+	csc_str_append(strA, longStr);
+	csc_str_append_ch(strA, 'k');
+	csc_str_append(strA, longStr);
+	csc_str_append_ch(strA, 'k');
+	csc_str_append(strA, longStr);
+	printPassFail( "append1", csc_streq(csc_str_charr(strA),
+										  longStr "k" longStr "k" longStr
+										  "k"
+										  longStr "k" longStr "k" longStr
+										));
+
+	csc_str_free(strA);
+	printPassFail("append1Free", csc_mck_nchunks()==0);
+
+	fclose(fout);
+}
+
+
+
