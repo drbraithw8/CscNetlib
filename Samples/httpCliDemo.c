0955460e4f569e5fdfe3d3d44e70428879924e2e
Stephen Braithwaite <drbraithw8@gmail.com>
2018-06-01 15:46:43 +1000

    Added httpDemo.c to Samples directory.

---
+++ Samples/httpCliDemo.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <unistd.h>
+#include <CscNetLib/netCli.h>
+#include <CscNetLib/std.h>
+
+#define LINE_MAX 99
+
+
+int main(int argc, char **argv)
+{   char line[LINE_MAX+1];
+    int isFin;
+    int lineNo;
+
+// Make the connection.
+    csc_cli_t *ntp = csc_cli_new();
+    int fdes0 = csc_cli_mkConn(ntp, "example.com", 80); assert(fdes0!=-1);
+    csc_cli_free(ntp);
+
+// Convert file descriptor to input and output streams.
+	int fdes1 = dup(fdes0);         assert(fdes1!=-1);
+    FILE *fin = fdopen(fdes0,"r");  assert(fin!=NULL);
+    FILE *fout = fdopen(fdes1,"w"); assert(fout!=NULL);
+
+// Data to send.
+    const char *sendData[] = 
+    { "GET /index.html HTTP/1.1"
+    , "Host: localhost" 
+    , "Connection: close" 
+    , "" 
+    };
+
+// Send a request.
+    lineNo = 0;
+    for (int i=0; i<csc_dim(sendData); i++)
+    {   fprintf(fout, "%s\n", sendData[i]);
+        fprintf(stdout, "Sent %3d \"%s\"\n", ++lineNo, sendData[i]);
+    }
+    fflush(fout);
+
+// Get the response.
+    isFin = csc_FALSE;
+    lineNo = 0;
+    while(!isFin)
+    {   int lineLen = csc_fgetline(fin, line, LINE_MAX);
+        if (lineLen < 0)
+        {   printf("EOF\n");
+            isFin = csc_TRUE;
+        }
+        else
+        {   printf("Got %3d \"%s\"\n", ++lineNo, line);
+            // if (csc_streq(line,""))
+            // {    isFin = csc_TRUE;
+            // }
+        }
+    }
+
+// Close input and output streams.
+    fclose(fin);
+    fclose(fout);
+
+// Bye.
+    exit(0);
+}
+
---
+++ Samples/httpDemo.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <CscNetLib/http.h>
+
+
+
+int main(int argc, char **argv)
+{   csc_httpErr_t errCode;
+
+// Resources.
+	csc_http_t *sendMsg = NULL;
+	csc_http_t *rcvMsg = NULL;
+
+// Create HTTP message.
+	sendMsg = csc_http_new();			assert(sendMsg!=NULL);
+	csc_http_addSF(sendMsg, csc_httpSF_method, "GET");
+	csc_http_addSF(sendMsg, csc_httpSF_reqUri, "/index.html");
+	csc_http_addSF(sendMsg, csc_httpSF_protocol, "HTTP/1.1");
+	csc_http_addHdr(sendMsg, "Host", "example.com");
+	csc_http_addHdr(sendMsg, "Connection", "close");
+
+// Add GET arguments.
+	csc_http_addUrlVal(sendMsg, "name", "Fred Blogs");
+	csc_http_addUrlVal(sendMsg, "age", "23");
+
+// Send HTTP message.
+	printf("%s", "------------- Begin send message ----------------\n");
+	errCode = csc_http_sendCliFILE(sendMsg, stdout);  assert(errCode==csc_httpErr_Ok);
+	printf("%s", "------------- End send message ----------------\n");
+
+// Create and receive the HTTP response.
+	rcvMsg = csc_http_new();							  assert(rcvMsg!=NULL);
+    const char *responseStr = 
+		"HTTP/1.1 200 OK\n"
+		"Accept-Ranges: bytes\n"
+		"Content-Type: text/html\n"
+		"Date: Fri, 01 Jun 2018 03:36:55 GMT\n"
+		"Last-Modified: Fri, 01 Jun 2018 03:29:13 GMT\n"
+		"Server: ECS (oxr/83C0)\n"
+		"Content-Length: 94\n"
+		"Connection: close\n"
+		"\n" ;
+	errCode = csc_http_rcvCliStr(rcvMsg, responseStr);    assert(errCode==csc_httpErr_Ok);
+	printf("Protocol: %s\n", csc_http_getSF(rcvMsg, csc_httpSF_protocol));
+	const char *statCode = csc_http_getSF(rcvMsg, csc_httpSF_statCode);
+	printf("StatCode: %s\n", statCode);
+	if (csc_streq(statCode,"200"))
+		printf("Content-Length: %s\n", csc_http_getHdr(rcvMsg, "Content-Length"));
+
+// Free resources.
+	if (sendMsg)
+		csc_http_free(sendMsg);
+	if (rcvMsg)
+		csc_http_free(rcvMsg);
+
+// Bye.
+    exit(0);
+}
+
